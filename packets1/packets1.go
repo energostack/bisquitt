9d8a8f24fa3e415de414715e4044ff3486d7ae61
Miroslav Prymek <prymek@gosw.cz>
2022-09-02 09:51:46 +0200

    packets1: Clean `Packet` interface up

--- packets1/packets1.go
+++ packets1/packets1.go
@@ -35,10 +35,18 @@ const MaxPacketLen = 8192
 const MaxPayloadLength = 7168
 
 type Packet interface {
-	SetVarPartLength(uint16)
+	fmt.Stringer
+
 	Write(io.Writer) error
 	Unpack(io.Reader) error
-	String() string
+
+	// NOTE: This method is not used anywhere but we must temporary keep it
+	// here because if we remove it, MQTT packets would accidently implement
+	// this interface and we would not be able to type-switch between MQTT
+	// and MQTT-SN packets.
+	// TODO: Remove as soon as this interface is changed and this problem
+	//  disappears.
+	SetVarPartLength(uint16)
 }
 
 // TopicID type constants.
