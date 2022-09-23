8cb6b6f45193381f33bdfac3c96dec2aab6ce335
Miroslav Prymek <prymek@gosw.cz>
2022-09-23 11:22:26 +0200

    packets1: Rename `WillMsgUpdate` to `WillMsgUpd`

--- packets1/willmsgupd_test.go
+++ packets1/willmsgupd_test.go
@@ -7,18 +7,18 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestWillMsgUpdateStruct(t *testing.T) {
+func TestWillMsgUpdStruct(t *testing.T) {
 	willMsg := []byte("test-msg")
-	pkt := NewWillMsgUpdate(willMsg)
+	pkt := NewWillMsgUpd(willMsg)
 
 	if assert.NotNil(t, pkt, "New packet should not be nil") {
-		assert.Equal(t, "*packets1.WillMsgUpdate", reflect.TypeOf(pkt).String(), "Type should be WillMsgUpdate")
+		assert.Equal(t, "*packets1.WillMsgUpd", reflect.TypeOf(pkt).String(), "Type should be WillMsgUpd")
 		assert.Equal(t, willMsg, pkt.WillMsg, "Bad WillMsg value")
 	}
 }
 
-func TestWillMsgUpdateMarshal(t *testing.T) {
-	pkt1 := NewWillMsgUpdate([]byte("test-message"))
+func TestWillMsgUpdMarshal(t *testing.T) {
+	pkt1 := NewWillMsgUpd([]byte("test-message"))
 	pkt2 := testPacketMarshal(t, pkt1)
-	assert.Equal(t, pkt1, pkt2.(*WillMsgUpdate))
+	assert.Equal(t, pkt1, pkt2.(*WillMsgUpd))
 }
