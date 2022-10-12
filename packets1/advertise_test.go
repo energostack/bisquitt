68cf81bba6180f72346882d84262d00044f69a33
Miroslav Prymek <prymek@gosw.cz>
2022-10-12 11:43:17 +0200

    packets1: Add full-coverage `Advertise` unit tests

--- packets1/advertise_test.go
+++ packets1/advertise_test.go
@@ -1,27 +1,69 @@
 package packets1
 
 import (
+	"bytes"
 	"reflect"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+
+	pkts "github.com/energomonitor/bisquitt/packets"
 )
 
-func TestAdvertiseStruct(t *testing.T) {
+func TestAdvertiseConstructor(t *testing.T) {
+	assert := assert.New(t)
+
 	gatewayID := uint8(12)
 	duration := uint16(123)
 	pkt := NewAdvertise(gatewayID, duration)
 
-	if assert.NotNil(t, pkt, "New packet should not be nil") {
-		assert.Equal(t, "*packets1.Advertise", reflect.TypeOf(pkt).String(), "Type should be Advertise")
-		assert.Equal(t, gatewayID, pkt.GatewayID, "Bad GatewayID")
-		assert.Equal(t, duration, pkt.Duration, "Bad Duration value")
-		assert.Equal(t, uint16(5), pkt.PacketLength(), "Length should be 5")
+	if pkt == nil {
+		t.Fatal("New packet should not be nil")
 	}
+
+	assert.Equal("*packets1.Advertise", reflect.TypeOf(pkt).String(), "Type should be Advertise")
+	assert.Equal(gatewayID, pkt.GatewayID, "Bad GatewayID")
+	assert.Equal(duration, pkt.Duration, "Bad Duration value")
+	assert.Equal(uint16(5), pkt.PacketLength(), "Length should be 5")
 }
 
 func TestAdvertiseMarshal(t *testing.T) {
 	pkt1 := NewAdvertise(12, 123)
 	pkt2 := testPacketMarshal(t, pkt1)
 	assert.Equal(t, pkt1, pkt2.(*Advertise))
+}
+
+func TestAdvertiseUnmarshalInvalid(t *testing.T) {
+	assert := assert.New(t)
+
+	// Packet too short.
+	buff := bytes.NewBuffer([]byte{
+		4,                    // Length
+		byte(pkts.ADVERTISE), // MsgType
+		0,                    // GwId
+		0,                    // Duration - MSB
+		// Duration - LSB missing
+	})
+	_, err := ReadPacket(buff)
+	if assert.Error(err) {
+		assert.Contains(err.Error(), "bad ADVERTISE packet length")
+	}
+
+	// Packet too long.
+	buff = bytes.NewBuffer([]byte{
+		6,                    // Length
+		byte(pkts.ADVERTISE), // MsgType
+		0,                    // GwId
+		0, 0,                 // Duration
+		0, // junk byte
+	})
+	_, err = ReadPacket(buff)
+	if assert.Error(err) {
+		assert.Contains(err.Error(), "bad ADVERTISE packet length")
+	}
+}
+
+func TestAdvertiseStringer(t *testing.T) {
+	pkt := NewAdvertise(12, 123)
+	assert.Equal(t, "ADVERTISE(GatewayID=12,Duration=123)", pkt.String())
 }
