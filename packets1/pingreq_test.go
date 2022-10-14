f6487b051c1fc5b4104aaec653c0675ec44b391d
Miroslav Prymek <prymek@gosw.cz>
2022-10-14 10:50:56 +0200

    packets1: Add full-coverage `Pingreq` unit tests

--- packets1/pingreq_test.go
+++ packets1/pingreq_test.go
@@ -7,18 +7,27 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestPingreqStruct(t *testing.T) {
+func TestPingreqConstructor(t *testing.T) {
+	assert := assert.New(t)
+
 	clientID := []byte("test-client")
 	pkt := NewPingreq(clientID)
 
-	if assert.NotNil(t, pkt, "New packet should not be nil") {
-		assert.Equal(t, "*packets1.Pingreq", reflect.TypeOf(pkt).String(), "Type should be Pingreq")
-		assert.Equal(t, clientID, pkt.ClientID, "Bad ClientID value")
+	if pkt == nil {
+		t.Fatal("New packet should not be nil")
 	}
+
+	assert.Equal("*packets1.Pingreq", reflect.TypeOf(pkt).String(), "Type should be Pingreq")
+	assert.Equal(clientID, pkt.ClientID, "Bad ClientID value")
 }
 
 func TestPingreqMarshal(t *testing.T) {
 	pkt1 := NewPingreq([]byte("test-client"))
 	pkt2 := testPacketMarshal(t, pkt1)
 	assert.Equal(t, pkt1, pkt2.(*Pingreq))
+}
+
+func TestPingreqStringer(t *testing.T) {
+	pkt := NewPingreq([]byte("test-client"))
+	assert.Equal(t, `PINGREQ(ClientID="test-client")`, pkt.String())
 }
