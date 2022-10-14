23f902fcd862fceec6b564169d019d5cba33b35d
Miroslav Prymek <prymek@gosw.cz>
2022-10-14 18:07:46 +0200

    packets1: Add full-coverage `WillTopic` unit tests

--- packets1/willtopic_test.go
+++ packets1/willtopic_test.go
@@ -1,28 +1,65 @@
 package packets1
 
 import (
+	"bytes"
 	"reflect"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+
+	pkts "github.com/energomonitor/bisquitt/packets"
 )
 
-func TestWillTopicStruct(t *testing.T) {
+func TestWillTopicConstructor(t *testing.T) {
+	assert := assert.New(t)
+
 	qos := uint8(1)
 	retain := false
 	willTopic := "test-topic"
 	pkt := NewWillTopic(willTopic, qos, retain)
 
-	if assert.NotNil(t, pkt, "New packet should not be nil") {
-		assert.Equal(t, "*packets1.WillTopic", reflect.TypeOf(pkt).String(), "Type should be WillTopic")
-		assert.Equal(t, qos, pkt.QOS, "Bad QOS value")
-		assert.Equal(t, retain, pkt.Retain, "Bad Retain flag value")
-		assert.Equal(t, willTopic, pkt.WillTopic, "Bad WillTopic value")
+	if pkt == nil {
+		t.Fatal("New packet should not be nil")
 	}
+
+	assert.Equal("*packets1.WillTopic", reflect.TypeOf(pkt).String(), "Type should be WillTopic")
+	assert.Equal(qos, pkt.QOS, "Bad QOS value")
+	assert.Equal(retain, pkt.Retain, "Bad Retain flag value")
+	assert.Equal(willTopic, pkt.WillTopic, "Bad WillTopic value")
 }
 
 func TestWillTopicMarshal(t *testing.T) {
 	pkt1 := NewWillTopic("test-topic", 1, true)
 	pkt2 := testPacketMarshal(t, pkt1)
 	assert.Equal(t, pkt1, pkt2.(*WillTopic))
+
+	// Packet with zero-length topic is a special case (does not contain Flags).
+	pkt1 = NewWillTopic("", 0, false)
+	pkt2 = testPacketMarshal(t, pkt1)
+	assert.Equal(t, pkt1, pkt2.(*WillTopic))
+}
+
+func TestWillTopicUnmarshalInvalid(t *testing.T) {
+	assert := assert.New(t)
+
+	// Packet too short.
+	buff := bytes.NewBuffer([]byte{
+		3,                    // Length
+		byte(pkts.WILLTOPIC), // MsgType
+		0,                    // Flags
+		// Will Topic missing
+	})
+	_, err := ReadPacket(buff)
+	if assert.Error(err) {
+		assert.Contains(err.Error(), "bad WILLTOPIC packet length")
+	}
+}
+
+func TestWillTopicStringer(t *testing.T) {
+	pkt := NewWillTopic("test-topic", 1, true)
+	assert.Equal(t, `WILLTOPIC(WillTopic="test-topic", QOS=1, Retain=true)`, pkt.String())
+
+	// Packet with zero-length topic.
+	pkt = NewWillTopic("", 0, false)
+	assert.Equal(t, `WILLTOPIC(WillTopic="")`, pkt.String())
 }
