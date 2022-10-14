fcc7b994687bf544e6e91e24031baf07b3dc9678
Miroslav Prymek <prymek@gosw.cz>
2022-10-14 15:59:23 +0200

    packets1: Add full-coverage `Register` unit tests

--- packets1/register_test.go
+++ packets1/register_test.go
@@ -1,28 +1,58 @@
 package packets1
 
 import (
+	"bytes"
 	"reflect"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+
+	pkts "github.com/energomonitor/bisquitt/packets"
 )
 
-func TestRegisterStruct(t *testing.T) {
+func TestRegisterConstructor(t *testing.T) {
+	assert := assert.New(t)
+
 	topicID := uint16(123)
 	topic := "test-topic"
 	pkt := NewRegister(topicID, topic)
 
-	if assert.NotNil(t, pkt, "New packet should not be nil") {
-		assert.Equal(t, "*packets1.Register", reflect.TypeOf(pkt).String(), "Type should be Register")
-		assert.Equal(t, topicID, pkt.TopicID, "Bad TopicID value")
-		assert.Equal(t, uint16(0), pkt.MessageID(), "Default MessageID should be 0")
-		assert.Equal(t, topic, pkt.TopicName, "Bad TopicName value")
+	if pkt == nil {
+		t.Fatal("New packet should not be nil")
 	}
+
+	assert.Equal("*packets1.Register", reflect.TypeOf(pkt).String(), "Type should be Register")
+	assert.Equal(topicID, pkt.TopicID, "Bad TopicID value")
+	assert.Equal(uint16(0), pkt.MessageID(), "Default MessageID should be 0")
+	assert.Equal(topic, pkt.TopicName, "Bad TopicName value")
 }
 
 func TestRegisterMarshal(t *testing.T) {
-	pkt1 := NewRegister(123, "test-topic")
-	pkt1.SetMessageID(12)
+	pkt1 := NewRegister(1234, "test-topic")
+	pkt1.SetMessageID(2345)
 	pkt2 := testPacketMarshal(t, pkt1)
 	assert.Equal(t, pkt1, pkt2.(*Register))
+}
+
+func TestRegisterUnmarshalInvalid(t *testing.T) {
+	assert := assert.New(t)
+
+	// Packet too short.
+	buff := bytes.NewBuffer([]byte{
+		6,                   // Length
+		byte(pkts.REGISTER), // MsgType
+		0, 1,                // Topic ID
+		0, 2, // Message ID
+		// TopicName missing
+	})
+	_, err := ReadPacket(buff)
+	if assert.Error(err) {
+		assert.Contains(err.Error(), "bad REGISTER packet length")
+	}
+}
+
+func TestRegisterStringer(t *testing.T) {
+	pkt := NewRegister(1234, "test-topic")
+	pkt.SetMessageID(2345)
+	assert.Equal(t, `REGISTER(TopicName="test-topic", TopicID=1234, MessageID=2345)`, pkt.String())
 }
