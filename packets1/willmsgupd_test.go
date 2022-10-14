065b49b7a5610cd450fddb3ad2f164993b2350ba
Miroslav Prymek <prymek@gosw.cz>
2022-10-14 17:42:55 +0200

    packets1: Add full-coverage `WillMsgUpd` unit tests

--- packets1/willmsgupd_test.go
+++ packets1/willmsgupd_test.go
@@ -7,18 +7,27 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestWillMsgUpdStruct(t *testing.T) {
-	willMsg := []byte("test-msg")
-	pkt := NewWillMsgUpd(willMsg)
+func TestWillMsgUpdConstructor(t *testing.T) {
+	assert := assert.New(t)
 
-	if assert.NotNil(t, pkt, "New packet should not be nil") {
-		assert.Equal(t, "*packets1.WillMsgUpd", reflect.TypeOf(pkt).String(), "Type should be WillMsgUpd")
-		assert.Equal(t, willMsg, pkt.WillMsg, "Bad WillMsg value")
+	data := []byte("test-data")
+	pkt := NewWillMsgUpd(data)
+
+	if pkt == nil {
+		t.Fatal("New packet should not be nil")
 	}
+
+	assert.Equal("*packets1.WillMsgUpd", reflect.TypeOf(pkt).String(), "Type should be WillMsgUpd")
+	assert.Equal(data, pkt.WillMsg, "Bad WillMsg value")
 }
 
 func TestWillMsgUpdMarshal(t *testing.T) {
 	pkt1 := NewWillMsgUpd([]byte("test-message"))
 	pkt2 := testPacketMarshal(t, pkt1)
 	assert.Equal(t, pkt1, pkt2.(*WillMsgUpd))
+}
+
+func TestWillMsgUpdStringer(t *testing.T) {
+	pkt := NewWillMsgUpd([]byte("test-data"))
+	assert.Equal(t, `WILLMSGUPD(WillMsg="test-data")`, pkt.String())
 }
