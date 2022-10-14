dd82f6e2b83dcf45d330bb3b6ecdd9698a27f5db
Miroslav Prymek <prymek@gosw.cz>
2022-10-14 11:03:48 +0200

    packets1: Add full-coverage `Puback` unit tests

--- packets1/puback_test.go
+++ packets1/puback_test.go
@@ -1,24 +1,31 @@
 package packets1
 
 import (
+	"bytes"
 	"fmt"
 	"reflect"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+
+	pkts "github.com/energomonitor/bisquitt/packets"
 )
 
-func TestPubackStruct(t *testing.T) {
+func TestPubackConstructor(t *testing.T) {
+	assert := assert.New(t)
+
 	topicID := uint16(123)
 	pkt := NewPuback(topicID, RC_ACCEPTED)
 
-	if assert.NotNil(t, pkt, "New packet should not be nil") {
-		assert.Equal(t, "*packets1.Puback", reflect.TypeOf(pkt).String(), "Type should be Puback")
-		assert.Equal(t, topicID, pkt.TopicID, fmt.Sprintf("TopicID should be %d", topicID))
-		assert.Equal(t, uint16(0), pkt.MessageID(), "Default MessageID should be 0")
-		assert.Equal(t, RC_ACCEPTED, pkt.ReturnCode, "ReturnCode should be RC_ACCEPTED")
-		assert.Equal(t, uint16(7), pkt.PacketLength(), "Default Length should be 2")
+	if pkt == nil {
+		t.Fatal("New packet should not be nil")
 	}
+
+	assert.Equal("*packets1.Puback", reflect.TypeOf(pkt).String(), "Type should be Puback")
+	assert.Equal(topicID, pkt.TopicID, fmt.Sprintf("TopicID should be %d", topicID))
+	assert.Equal(uint16(0), pkt.MessageID(), "Default MessageID should be 0")
+	assert.Equal(RC_ACCEPTED, pkt.ReturnCode, "ReturnCode should be RC_ACCEPTED")
+	assert.Equal(uint16(7), pkt.PacketLength(), "Default Length should be 2")
 }
 
 func TestPubackMarshal(t *testing.T) {
@@ -26,4 +33,41 @@ func TestPubackMarshal(t *testing.T) {
 	pkt1.SetMessageID(12)
 	pkt2 := testPacketMarshal(t, pkt1)
 	assert.Equal(t, pkt1, pkt2.(*Puback))
+}
+
+func TestPubackUnmarshalInvalid(t *testing.T) {
+	assert := assert.New(t)
+
+	// Packet too short.
+	buff := bytes.NewBuffer([]byte{
+		6,                 // Length
+		byte(pkts.PUBACK), // MsgType
+		0, 1,              // Topic ID
+		0, 2, // Message ID
+		// Return Code missing
+	})
+	_, err := ReadPacket(buff)
+	if assert.Error(err) {
+		assert.Contains(err.Error(), "bad PUBACK packet length")
+	}
+
+	// Packet too long.
+	buff = bytes.NewBuffer([]byte{
+		6,                 // Length
+		byte(pkts.PUBACK), // MsgType
+		0, 1,              // Topic ID
+		0, 2, // Message ID
+		3, // Return Code
+		0, // junk
+	})
+	_, err = ReadPacket(buff)
+	if assert.Error(err) {
+		assert.Contains(err.Error(), "bad PUBACK packet length")
+	}
+}
+
+func TestPubackStringer(t *testing.T) {
+	pkt := NewPuback(123, RC_CONGESTION)
+	pkt.SetMessageID(12)
+	assert.Equal(t, "PUBACK(TopicID=123, ReturnCode=1, MessageID=12)", pkt.String())
 }
