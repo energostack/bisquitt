bd01ff0cd55108fe1915ba29cb39ac7995b43476
Miroslav Prymek <prymek@gosw.cz>
2022-10-14 18:08:04 +0200

    packets1: Add full-coverage `WillMsgReq` unit tests

--- packets1/willmsgreq_test.go
+++ packets1/willmsgreq_test.go
@@ -1,22 +1,47 @@
 package packets1
 
 import (
+	"bytes"
 	"reflect"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+
+	pkts "github.com/energomonitor/bisquitt/packets"
 )
 
-func TestWillMsgReqStruct(t *testing.T) {
+func TestWillMsgReqConstructor(t *testing.T) {
 	pkt := NewWillMsgReq()
 
-	if assert.NotNil(t, pkt, "New packet should not be nil") {
-		assert.Equal(t, "*packets1.WillMsgReq", reflect.TypeOf(pkt).String(), "Type should be WillMsgReq")
+	if pkt == nil {
+		t.Fatal("New packet should not be nil")
 	}
+
+	assert.Equal(t, "*packets1.WillMsgReq", reflect.TypeOf(pkt).String(), "Type should be WillMsgReq")
 }
 
 func TestWillMsgReqMarshal(t *testing.T) {
 	pkt1 := NewWillMsgReq()
 	pkt2 := testPacketMarshal(t, pkt1)
 	assert.Equal(t, pkt1, pkt2.(*WillMsgReq))
+}
+
+func TestWillMsgReqUnmarshalInvalid(t *testing.T) {
+	assert := assert.New(t)
+
+	// Packet too long.
+	buff := bytes.NewBuffer([]byte{
+		3,                     // Length
+		byte(pkts.WILLMSGREQ), // MsgType
+		0,                     // junk
+	})
+	_, err := ReadPacket(buff)
+	if assert.Error(err) {
+		assert.Contains(err.Error(), "bad WILLMSGREQ packet length")
+	}
+}
+
+func TestWillMsgReqStringer(t *testing.T) {
+	pkt := NewWillMsgReq()
+	assert.Equal(t, "WILLMSGREQ", pkt.String())
 }
