b67cd302ba50e81f822b454a74ba595bfa931559
Miroslav Prymek <prymek@gosw.cz>
2022-10-14 18:08:32 +0200

    packets1: Add full-coverage `WillTopicResp` unit tests

--- packets1/willtopicresp_test.go
+++ packets1/willtopicresp_test.go
@@ -1,23 +1,62 @@
 package packets1
 
 import (
+	"bytes"
 	"reflect"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+
+	pkts "github.com/energomonitor/bisquitt/packets"
 )
 
-func TestWillTopicRespStruct(t *testing.T) {
-	pkt := NewWillTopicResp(RC_ACCEPTED)
+func TestWillTopicRespConstructor(t *testing.T) {
+	assert := assert.New(t)
 
-	if assert.NotNil(t, pkt, "New packet should not be nil") {
-		assert.Equal(t, "*packets1.WillTopicResp", reflect.TypeOf(pkt).String(), "Type should be WillTopicResp")
-		assert.Equal(t, RC_ACCEPTED, pkt.ReturnCode, "ReturnCode should be RC_ACCEPTED")
+	pkt := NewWillTopicResp(RC_CONGESTION)
+
+	if pkt == nil {
+		t.Fatal("New packet should not be nil")
 	}
+
+	assert.Equal("*packets1.WillTopicResp", reflect.TypeOf(pkt).String(), "Type should be WillTopicResp")
+	assert.Equal(RC_CONGESTION, pkt.ReturnCode, "ReturnCode should be RC_CONGESTION")
 }
 
 func TestWillTopicRespMarshal(t *testing.T) {
 	pkt1 := NewWillTopicResp(RC_CONGESTION)
 	pkt2 := testPacketMarshal(t, pkt1)
 	assert.Equal(t, pkt1, pkt2.(*WillTopicResp))
+}
+
+func TestWillTopicRespUnmarshalInvalid(t *testing.T) {
+	assert := assert.New(t)
+
+	// Packet too short.
+	buff := bytes.NewBuffer([]byte{
+		2,                        // Length
+		byte(pkts.WILLTOPICRESP), // MsgType
+		// Return Code missing
+	})
+	_, err := ReadPacket(buff)
+	if assert.Error(err) {
+		assert.Contains(err.Error(), "bad WILLTOPICRESP packet length")
+	}
+
+	// Packet too long.
+	buff = bytes.NewBuffer([]byte{
+		4,                        // Length
+		byte(pkts.WILLTOPICRESP), // MsgType
+		0,                        // Return Code
+		0,                        // junk
+	})
+	_, err = ReadPacket(buff)
+	if assert.Error(err) {
+		assert.Contains(err.Error(), "bad WILLTOPICRESP packet length")
+	}
+}
+
+func TestWillTopicRespStringer(t *testing.T) {
+	pkt := NewWillTopicResp(RC_CONGESTION)
+	assert.Equal(t, "WILLTOPICRESP(ReturnCode=1)", pkt.String())
 }
