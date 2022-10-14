6c71a0a46b562cf62d29fbc6eda658a1ee7f4344
Miroslav Prymek <prymek@gosw.cz>
2022-10-14 15:52:32 +0200

    packets1: Add full-coverage `Regack` unit tests

--- packets1/regack_test.go
+++ packets1/regack_test.go
@@ -1,29 +1,73 @@
 package packets1
 
 import (
+	"bytes"
 	"reflect"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+
+	pkts "github.com/energomonitor/bisquitt/packets"
 )
 
-func TestRegackStruct(t *testing.T) {
+func TestRegackConstructor(t *testing.T) {
+	assert := assert.New(t)
+
 	topicID := uint16(123)
 	returnCode := RC_ACCEPTED
 	pkt := NewRegack(topicID, returnCode)
 
-	if assert.NotNil(t, pkt, "New packet should not be nil") {
-		assert.Equal(t, "*packets1.Regack", reflect.TypeOf(pkt).String(), "Type should be Regack")
-		assert.Equal(t, uint16(7), pkt.PacketLength(), "Default Length should be 7")
-		assert.Equal(t, topicID, pkt.TopicID, "Bad TopicID value")
-		assert.Equal(t, uint16(0), pkt.MessageID(), "Default MessageID should be 0")
-		assert.Equal(t, returnCode, pkt.ReturnCode, "ReturnCode should be RC_ACCEPTED")
+	if pkt == nil {
+		t.Fatal("New packet should not be nil")
 	}
+
+	assert.Equal("*packets1.Regack", reflect.TypeOf(pkt).String(), "Type should be Regack")
+	assert.Equal(uint16(7), pkt.PacketLength(), "Default Length should be 7")
+	assert.Equal(topicID, pkt.TopicID, "Bad TopicID value")
+	assert.Equal(uint16(0), pkt.MessageID(), "Default MessageID should be 0")
+	assert.Equal(returnCode, pkt.ReturnCode, "ReturnCode should be RC_ACCEPTED")
 }
 
 func TestRegackMarshal(t *testing.T) {
-	pkt1 := NewRegack(123, RC_CONGESTION)
-	pkt1.SetMessageID(12)
+	pkt1 := NewRegack(1234, RC_CONGESTION)
+	pkt1.SetMessageID(2345)
 	pkt2 := testPacketMarshal(t, pkt1)
 	assert.Equal(t, pkt1, pkt2.(*Regack))
+}
+
+func TestRegackUnmarshalInvalid(t *testing.T) {
+	assert := assert.New(t)
+
+	// Packet too short.
+	buff := bytes.NewBuffer([]byte{
+		6,                 // Length
+		byte(pkts.REGACK), // MsgType
+		0, 1,              // Topic ID
+		0, 2, // Message ID
+		// Return Code missing
+	})
+	_, err := ReadPacket(buff)
+	if assert.Error(err) {
+		assert.Contains(err.Error(), "bad REGACK packet length")
+	}
+
+	// Packet too long.
+	buff = bytes.NewBuffer([]byte{
+		8,                 // Length
+		byte(pkts.REGACK), // MsgType
+		0, 1,              // Topic ID
+		0, 2, // Message ID
+		1, // Return Code
+		0, // junk
+	})
+	_, err = ReadPacket(buff)
+	if assert.Error(err) {
+		assert.Contains(err.Error(), "bad REGACK packet length")
+	}
+}
+
+func TestRegackStringer(t *testing.T) {
+	pkt := NewRegack(1234, RC_CONGESTION)
+	pkt.SetMessageID(2345)
+	assert.Equal(t, "REGACK(TopicID=1234, ReturnCode=1, MessageID=2345)", pkt.String())
 }
