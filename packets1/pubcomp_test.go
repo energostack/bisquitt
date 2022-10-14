227cc3bafd5c9b963ddeb01a3e9146b3bb9bafc3
Miroslav Prymek <prymek@gosw.cz>
2022-10-14 15:28:55 +0200

    packets1: Add full-coverage `Pubcomp` unit tests

--- packets1/pubcomp_test.go
+++ packets1/pubcomp_test.go
@@ -1,24 +1,66 @@
 package packets1
 
 import (
+	"bytes"
 	"reflect"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+
+	pkts "github.com/energomonitor/bisquitt/packets"
 )
 
-func TestPubcompStruct(t *testing.T) {
+func TestPubcompConstructor(t *testing.T) {
+	assert := assert.New(t)
+
 	pkt := NewPubcomp()
 
-	if assert.NotNil(t, pkt, "New packet should not be nil") {
-		assert.Equal(t, "*packets1.Pubcomp", reflect.TypeOf(pkt).String(), "Type should be Pubcomp")
-		assert.Equal(t, uint16(4), pkt.PacketLength(), "Default Length should be 4")
-		assert.Equal(t, uint16(0), pkt.MessageID(), "Default MessageID should be 0")
+	if pkt == nil {
+		t.Fatal("New packet should not be nil")
 	}
+
+	assert.Equal("*packets1.Pubcomp", reflect.TypeOf(pkt).String(), "Type should be Pubcomp")
+	assert.Equal(uint16(4), pkt.PacketLength(), "Default Length should be 4")
+	assert.Equal(uint16(0), pkt.MessageID(), "Default MessageID should be 0")
 }
 
 func TestPubcompMarshal(t *testing.T) {
 	pkt1 := NewPubcomp()
+	pkt1.SetMessageID(1234)
 	pkt2 := testPacketMarshal(t, pkt1)
 	assert.Equal(t, pkt1, pkt2.(*Pubcomp))
+}
+
+func TestPubcompUnmarshalInvalid(t *testing.T) {
+	assert := assert.New(t)
+
+	// Packet too short.
+	buff := bytes.NewBuffer([]byte{
+		3,                  // Length
+		byte(pkts.PUBCOMP), // MsgType
+		0,                  // Message ID MSB
+		// Message ID LSB missing
+	})
+	_, err := ReadPacket(buff)
+	if assert.Error(err) {
+		assert.Contains(err.Error(), "bad PUBCOMP packet length")
+	}
+
+	// Packet too long.
+	buff = bytes.NewBuffer([]byte{
+		5,                  // Length
+		byte(pkts.PUBCOMP), // MsgType
+		0, 1,               // Message ID
+		0, // junk
+	})
+	_, err = ReadPacket(buff)
+	if assert.Error(err) {
+		assert.Contains(err.Error(), "bad PUBCOMP packet length")
+	}
+}
+
+func TestPubcompStringer(t *testing.T) {
+	pkt := NewPubcomp()
+	pkt.SetMessageID(1234)
+	assert.Equal(t, "PUBCOMP(MessageID=1234)", pkt.String())
 }
