9878e6f07735367862abef4ef679f64effe58fad
Miroslav Prymek <prymek@gosw.cz>
2022-10-14 10:54:26 +0200

    packets1: Add full-coverage `Pingresp` unit tests

--- packets1/pingresp_test.go
+++ packets1/pingresp_test.go
@@ -7,17 +7,24 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestPingrespStruct(t *testing.T) {
+func TestPingrespConstructor(t *testing.T) {
 	pkt := NewPingresp()
 
-	if assert.NotNil(t, pkt, "New packet should not be nil") {
-		assert.Equal(t, "*packets1.Pingresp", reflect.TypeOf(pkt).String(), "Type should be Pingresp")
-		assert.Equal(t, uint16(2), pkt.PacketLength(), "Default Length should be 2")
+	if pkt == nil {
+		t.Fatal("New packet should not be nil")
 	}
+
+	assert.Equal(t, "*packets1.Pingresp", reflect.TypeOf(pkt).String(), "Type should be Pingresp")
+	assert.Equal(t, uint16(2), pkt.PacketLength(), "Default Length should be 2")
 }
 
 func TestPingrespMarshal(t *testing.T) {
 	pkt1 := NewPingresp()
 	pkt2 := testPacketMarshal(t, pkt1)
 	assert.Equal(t, pkt1, pkt2.(*Pingresp))
+}
+
+func TestPingrespStringer(t *testing.T) {
+	pkt := NewPingresp()
+	assert.Equal(t, "PINGRESP", pkt.String())
 }
