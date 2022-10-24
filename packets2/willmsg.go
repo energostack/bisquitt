c2c3d09b225e0b66d2228c737a04235a2cc94e48
Miroslav Prymek <prymek@gosw.cz>
2022-10-24 10:57:08 +0200

    packets2: Add `WillMsg` packet implementation

---
+++ packets2/willmsg.go
@@ -0,0 +1,47 @@
+package packets2
+
+import (
+	"fmt"
+
+	pkts "github.com/energomonitor/bisquitt/packets"
+)
+
+type WillMsg struct {
+	pkts.Header
+	PacketV2
+	// Fields
+	WillMsg []byte
+}
+
+// NOTE: Packet length is initialized in this constructor and recomputed in m.Write().
+func NewWillMsg(willMsg []byte) *WillMsg {
+	p := &WillMsg{
+		Header:  *pkts.NewHeader(pkts.WILLMSG, 0),
+		WillMsg: willMsg,
+	}
+	p.computeLength()
+	return p
+}
+
+func (p *WillMsg) computeLength() {
+	msgLength := len(p.WillMsg)
+	p.Header.SetVarPartLength(uint16(msgLength))
+}
+
+func (p *WillMsg) Pack() ([]byte, error) {
+	p.computeLength()
+	buf := p.Header.PackToBuffer()
+
+	_, _ = buf.Write(p.WillMsg)
+
+	return buf.Bytes(), nil
+}
+
+func (p *WillMsg) Unpack(buf []byte) error {
+	p.WillMsg = buf
+	return nil
+}
+
+func (p WillMsg) String() string {
+	return fmt.Sprintf("WILLMSG2(WillMsg=%#v)", string(p.WillMsg))
+}
