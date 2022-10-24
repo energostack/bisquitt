6791e712b8db99702903ad8e46c67d299bcf563f
Miroslav Prymek <prymek@gosw.cz>
2022-10-24 10:57:44 +0200

    packets2: Add `WillMsgUpd` packet implementation

---
+++ packets2/willmsgupd_test.go
@@ -0,0 +1,33 @@
+package packets2
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestWillMsgUpdConstructor(t *testing.T) {
+	assert := assert.New(t)
+
+	data := []byte("test-data")
+	pkt := NewWillMsgUpd(data)
+
+	if pkt == nil {
+		t.Fatal("New packet should not be nil")
+	}
+
+	assert.Equal("*packets2.WillMsgUpd", reflect.TypeOf(pkt).String(), "Type should be WillMsgUpd")
+	assert.Equal(data, pkt.WillMsg, "Bad WillMsg value")
+}
+
+func TestWillMsgUpdMarshal(t *testing.T) {
+	pkt1 := NewWillMsgUpd([]byte("test-message"))
+	pkt2 := testPacketMarshal(t, pkt1)
+	assert.Equal(t, pkt1, pkt2.(*WillMsgUpd))
+}
+
+func TestWillMsgUpdStringer(t *testing.T) {
+	pkt := NewWillMsgUpd([]byte("test-data"))
+	assert.Equal(t, `WILLMSGUPD2(WillMsg="test-data")`, pkt.String())
+}
