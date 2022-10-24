1bf7998f5bea939e3115547f9784f24eefad3852
Miroslav Prymek <prymek@gosw.cz>
2022-10-24 10:56:33 +0200

    packets2: Add `Suback` packet implementation

--- packets2/packets2.go
+++ packets2/packets2.go
@@ -145,8 +145,8 @@ func NewPacketWithHeader(h pkts.Header) (pkt pkts.Packet, err error) {
 		pkt = &Pubrel{Header: h}
 	//case pkts.SUBSCRIBE:
 	//	pkt = &Subscribe{Header: h}
-	//case pkts.SUBACK:
-	//	pkt = &Suback{Header: h}
+	case pkts.SUBACK:
+		pkt = &Suback{Header: h}
 	//case pkts.UNSUBSCRIBE:
 	//	pkt = &Unsubscribe{Header: h}
 	//case pkts.UNSUBACK:
