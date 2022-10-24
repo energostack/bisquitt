4411489ca38101ac3a9e4cb7b0292290b82b1695
Miroslav Prymek <prymek@gosw.cz>
2022-10-24 10:50:59 +0200

    packets2: Add `Connect` packet implementation

--- packets2/packets2.go
+++ packets2/packets2.go
@@ -117,8 +117,8 @@ func NewPacketWithHeader(h pkts.Header) (pkt pkts.Packet, err error) {
 		pkt = &GwInfo{Header: h}
 	case pkts.AUTH:
 		pkt = &Auth{Header: h}
-	//case pkts.CONNECT:
-	//	pkt = &Connect{Header: h}
+	case pkts.CONNECT:
+		pkt = &Connect{Header: h}
 	case pkts.CONNACK:
 		pkt = &Connack{Header: h}
 	//case pkts.WILLTOPICREQ:
