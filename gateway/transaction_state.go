d07b1210e429c8652493e7e130294b1d16f603cf
Miroslav Prymek <prymek@gosw.cz>
2022-11-03 18:28:38 +0100

    gateway: Add `Stringer` implementation for `transactionState`
    Needed to print human-readable transactionState in logs.

--- gateway/transaction_state.go
+++ gateway/transaction_state.go
@@ -1,5 +1,7 @@
 package gateway
 
+import "fmt"
+
 // Transactions state constants.
 type transactionState int
 
@@ -11,3 +13,22 @@ const (
 	awaitingPubrel
 	awaitingPubcomp
 )
+
+func (s transactionState) String() string {
+	switch s {
+	case transactionDone:
+		return "done"
+	case awaitingRegack:
+		return "awaitingRegack"
+	case awaitingPuback:
+		return "awaitingPuback"
+	case awaitingPubrec:
+		return "awaitingPubrec"
+	case awaitingPubrel:
+		return "awaitingPubrel"
+	case awaitingPubcomp:
+		return "awaitingPubcomp"
+	default:
+		return fmt.Sprintf("invalid(%d)", s)
+	}
+}
